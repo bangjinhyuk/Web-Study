2d3a9a853c746566a70ce09efb9c70ef57b5dae0
bangjinhyuk <jinhyuk0103@gmail.com>
2022-07-24 14:36:13 +0900

    init project

---
+++ thymeleaf-study/src/test/java/com/example/webthymeleaf/WebThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webthymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebThymeleafApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
